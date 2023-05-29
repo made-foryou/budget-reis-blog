ab19674433a15f27285d60ca9027370a9927ffb8
Menno Tempelaar <menno@made-foryou.nl>
2023-05-29 12:44:14 +0200

    Implemented a first setup for the landing page

---
+++ config/budget-reis.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'default_title' => env('BUDGET_REIS_DEFAULT_TITLE') ?? env('APP_NAME'),
+    'default_description' => env('BUDGET_REIS_DEFAULT_DESCRIPTION') ?? env('APP_NAME'),
+];
