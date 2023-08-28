47fc0787139905148cf8c4982f81caa13a1adabf
Menno Tempelaar <menno@made-foryou.nl>
2023-08-28 15:13:59 +0800

    implement new is_visible column and factory and query builder.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,35 +5,45 @@ namespace App\Models;
 use App\Events\SavingModel;
 use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Database\Factories\CategoryFactory;
+use App\QueryBuilders\CategoryQueryBuilder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * @property-read int $id
  * @property string $name
  * @property string $slug
  * @property string $description
+ * @property boolean $is_visible
  * @property-read Carbon $created_at
  * @property-read Carbon $updated_at
  * @property-read Carbon|null $deleted_at
  * @property-read Category|null $category
  * @property-read Collection<Category> $categories
  * @property-read Collection<Post> $posts
+ *
+ * @method static CategoryQueryBuilder query()
+ * @method static CategoryFactory factory($count = null, $state = [])
  */
 class Category extends Model implements GeneratesASlug
 {
     use SoftDeletes;
     use GeneratesSlug;
+    use HasFactory;
 
     protected $fillable = [
         'name',
         'slug',
-        'description'
+        'description',
+        'is_visible',
     ];
 
     protected $casts = [
+        'is_visible' => 'boolean',
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
         'deleted_at' => 'datetime',
@@ -61,5 +71,10 @@ class Category extends Model implements GeneratesASlug
     public function getValueKey(): string
     {
         return 'name';
+    }
+
+    public function newEloquentBuilder($query): CategoryQueryBuilder
+    {
+        return new CategoryQueryBuilder($query);
     }
 }
