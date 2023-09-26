65bdd1ba75e1d2960380c4a0bcfb12cb2b28d5be
Menno Tempelaar <menno@made-foryou.nl>
2023-09-26 13:00:43 +0800

    implement the describable morph-to relation to the other models.

--- app/Models/Meta.php
+++ app/Models/Meta.php
@@ -6,10 +6,14 @@ use Illuminate\Support\Carbon;
 use Database\Factories\MetaFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * @property-read int $id
+ * @property-read class-string $describable_type
+ * @property-read int $describable_id
+ * @property-read Category|Page|Post $describable
  * @property-read Carbon $created_at
  * @property-read Carbon $updated_at
  * @property-read Carbon|null $deleted_at
@@ -40,6 +44,11 @@ class Meta extends Model
         'updated_at' => 'datetime',
         'deleted_at' => 'datetime',
     ];
+
+    public function describable(): MorphTo
+    {
+        return $this->morphTo('describable');
+    }
 
     protected static function newFactory(): MetaFactory
     {
--- database/migrations/2023_09_26_123257_create_meta_table.php
+++ database/migrations/2023_09_26_123257_create_meta_table.php
@@ -10,6 +10,9 @@ return new class extends Migration {
         Schema::create('meta', function (Blueprint $table) {
             $table->id();
 
+            $table->string('describable_type');
+            $table->unsignedBigInteger('describable_id');
+
             $table->string('title');
             $table->text('description');
 
